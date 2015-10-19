9df6b15a224d3335148d2fded008004f30dcfd9e
AmazonEchoObliqueStrategies.github.com <AmazonEchoObliqueStrategies.github.com@ma7.org>
2015-10-19 13:33:41 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,98 @@
+// Alexa SDK for JavaScript v1.0.00
+// Copyright (c) 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved. Use is subject to license terms.
+
+/**
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to create a Lambda function for handling Alexa Skill requests.
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, ask Space Geek for a space fact"
+ *  Alexa: "Here's your space fact: ..."
+ */
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+/**
+ * Array containing space facts.
+ */
+/**
+ * Oblique Strategies for Programmers
+ * https://github.com/RobBlackwell/oblique-strategies-for-programmers
+ * used with permission from Rob Blackwell
+ */
+var OBLIQUE_STRATEGIES = require('./ObliqueStrategies');
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+
+/**
+ * SpaceGeek is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var SpaceGeek = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+SpaceGeek.prototype = Object.create(AlexaSkill.prototype);
+SpaceGeek.prototype.constructor = SpaceGeek;
+
+SpaceGeek.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("SpaceGeek onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+SpaceGeek.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("SpaceGeek onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    handleNewFactRequest(response);
+};
+
+/**
+ * Overridden to show that a subclass can override this function to teardown session state.
+ */
+SpaceGeek.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("SpaceGeek onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+SpaceGeek.prototype.intentHandlers = {
+    GetNewFactIntent: function (intent, session, response) {
+        handleNewFactRequest(response);
+    },
+
+    HelpIntent: function (intent, session, response) {
+        response.ask("You can ask Space Geek tell me a space fact, or, you can say exit... What can I help you with?");
+    }
+};
+
+/**
+ * Gets a random new fact from the list and returns to the user.
+ */
+function handleNewFactRequest(response) {
+    // Get a random space fact from the space facts list
+    var factIndex = Math.floor(Math.random() * OBLIQUE_STRATEGIES.length);
+    var fact = OBLIQUE_STRATEGIES[factIndex];
+
+    // Create speech output
+    var speechOutput =  fact;
+
+    response.tellWithCard(speechOutput, "Eno", speechOutput);
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the SpaceGeek skill.
+    var spaceGeek = new SpaceGeek();
+    spaceGeek.execute(event, context);
+};
+
